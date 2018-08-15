d795f3fb4458ca5807053db4dcfe7894a15dd031
Tobias Lückel <lueckel@solutiondrive.de>
2018-08-15 18:39:42 +0200

    Added a step to stop the docker container after test execution

--- docker-test.sh
+++ docker-test.sh
@@ -2,3 +2,4 @@
 
 DOCKER_CONTAINER_ID=`docker run -d solutiondrive/docker-shopware-container:shopware$SHOPWARE_VERSION-php$PHP_VERSION`
 inspec exec tests/inspec/shopware-container -t docker://${DOCKER_CONTAINER_ID}
+docker stop ${DOCKER_CONTAINER_ID}
