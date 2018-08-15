aa9e61ba171b852fc74ab6cdc609ac806ec0755b
Tobias Lückel <lueckel@solutiondrive.de>
2018-08-15 21:55:15 +0200

    Added a spec to verify that the correct project directory exists

--- tests/inspec/shopware-container/controls/project_directory.rb
+++ tests/inspec/shopware-container/controls/project_directory.rb
@@ -1,3 +1,13 @@
+shopware_short_version = attribute(
+    'shopware_short_version',
+    description: 'Shopware short version'
+)
+
+php_short_version = attribute(
+    'php_short_version',
+    description: 'php short version'
+)
+
 control 'project directory' do
     impact 1.0
     title 'verifies project directory'
@@ -7,6 +17,11 @@ control 'project directory' do
     '
 
     describe directory('/var/www') do
+        it { should exist }
+        it { should be_directory }
+    end
+
+    describe directory('/var/www/shopware' + shopware_short_version.to_s + '_php' + php_short_version.to_s) do
         it { should exist }
         it { should be_directory }
     end
